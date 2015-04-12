be3e0c596d15da5704be6fc5dd2834e3d099005c
jsryu21 <jsryu21@snu.ac.kr>
2015-04-12 21:54:12 +0900

    Updated.

--- hw1/ga.cpp
+++ hw1/ga.cpp
@@ -11,6 +11,8 @@
 #define CALL_MEMBER_FN(object,ptrToMember)  ((object).*(ptrToMember))
 
 const static int PSIZE = 100; // Size of the population
+const static int ROULETTE_SELECTION_PRESSURE = 3; // 3 ~ 4
+const static double TOURNAMENT_SELECTION_PRESSURE = 0.5;
 
 struct Solution
 {
@@ -40,6 +42,7 @@ class SteadyStateGA {
     public:
         typedef void (SteadyStateGA::*EvaluateFn)(Solution& s);
         typedef void (SteadyStateGA::*GenerateRandomSolutionFn)(Solution& s);
+        typedef void (SteadyStateGA::*PreprocessFn)();
         typedef void (SteadyStateGA::*SelectionFn)(Solution& s);
         typedef void (SteadyStateGA::*CrossoverFn)(const Solution& p1, const Solution& p2, Solution& c);
         typedef void (SteadyStateGA::*MutationFn)(Solution& s);
@@ -47,13 +50,18 @@ class SteadyStateGA {
         SteadyStateGA(const TestCase& testCase
                 , EvaluateFn Evaluate_
                 , GenerateRandomSolutionFn GenerateRandomSolution_
+                , PreprocessFn Preprocess_
                 , SelectionFn Selection_
                 , CrossoverFn Crossover_
                 , MutationFn Mutation_
                 , ReplacementFn Replacement_);
         void Evaluate1(Solution& s);
         void GenerateRandomSolution1(Solution& s);
+        void Preprocess1();
         void Selection1(Solution& s);
+        void Selection2(Solution& s);
+        void Selection3(Solution& s);
+        void Selection4(Solution& s);
         void Crossover1(const Solution& p1, const Solution& p2, Solution& c);
         void Mutation1(Solution& s);
         void Replacement1(const Solution& offspr);
@@ -63,11 +71,13 @@ class SteadyStateGA {
     private:
         EvaluateFn Evaluate;
         GenerateRandomSolutionFn GenerateRandomSolution;
+        PreprocessFn Preprocess;
         SelectionFn Selection;
         CrossoverFn Crossover;
         MutationFn Mutation;
         ReplacementFn Replacement;
         void PrintSolution(const Solution& s);
+        void Normalize(Solution& s);
 
         int solutionLen;
         const std::vector< std::vector< double > >& solutionDist;
@@ -75,11 +85,16 @@ class SteadyStateGA {
         // population of solutions
         std::vector< Solution > population;
         Solution record;
+        Solution randomSolution;
+        Solution tempSolution;
+        std::vector< double > adjustedFitnesses;
+        double sumOfFitnesses;
 };
 
 SteadyStateGA::SteadyStateGA(const TestCase& testCase
         , EvaluateFn Evaluate_
         , GenerateRandomSolutionFn GenerateRandomSolution_
+        , PreprocessFn Preprocess_
         , SelectionFn Selection_
         , CrossoverFn Crossover_
         , MutationFn Mutation_
@@ -88,13 +103,21 @@ SteadyStateGA::SteadyStateGA(const TestCase& testCase
     timeLimit(testCase.TimeLimit),
     population(PSIZE, Solution(solutionLen)),
     record(solutionLen),
+    randomSolution(solutionLen),
+    tempSolution(solutionLen),
+    adjustedFitnesses(PSIZE),
+    sumOfFitnesses(0),
     Evaluate(Evaluate_),
     GenerateRandomSolution(GenerateRandomSolution_),
+    Preprocess(Preprocess_),
     Selection(Selection_),
     Crossover(Crossover_),
     Mutation(Mutation_),
     Replacement(Replacement_)
 {
+    for (int i = 0; i < solutionLen; ++i) {
+        randomSolution.Chromosome[i] = i;
+    }
 }
 
 // calculate the fitness of s and store it into s->f
@@ -111,14 +134,28 @@ void SteadyStateGA::Evaluate1(Solution& s) {
 // generate a random order-based solution at s
 void SteadyStateGA::GenerateRandomSolution1(Solution& s) {
     for (int i = 0; i < solutionLen; ++i) {
-        s.Chromosome[i] = i;
+        int r = std::rand() % solutionLen;
+        std::swap(randomSolution.Chromosome[i], randomSolution.Chromosome[r]);
     }
-    for (int i = 0; i < solutionLen; ++i) {
-        int r = i + std::rand() % (solutionLen - i);	// r is a random number in [i..N-i)
-        std::swap(s.Chromosome[i], s.Chromosome[r]); // swap
-    }
+    std::vector< int >::iterator zeroIter = std::find(randomSolution.Chromosome.begin(), randomSolution.Chromosome.end(), 0);
+    std::copy(randomSolution.Chromosome.begin(), zeroIter, std::copy(zeroIter, randomSolution.Chromosome.end(), s.Chromosome.begin()));
     // calculate the fitness
     CALL_MEMBER_FN(*this, Evaluate)(s);
+}
+
+void SteadyStateGA::Preprocess1() {
+    double maxFitness = 0;
+    double minFitness = std::numeric_limits< double >::max();
+    for (int i = 0; i < PSIZE; ++i) {
+        maxFitness = std::max(maxFitness, population[i].Fitness);
+        minFitness = std::min(minFitness, population[i].Fitness);
+    }
+    sumOfFitnesses = 0;
+    for (int i = 0; i < PSIZE; ++i) {
+        double adjustedFitness = (maxFitness - population[i].Fitness) + (maxFitness - minFitness) / (ROULETTE_SELECTION_PRESSURE - 1);
+        sumOfFitnesses += adjustedFitness;
+        adjustedFitnesses[i] = adjustedFitness;
+    }
 }
 
 // choose one solution from the population
@@ -128,18 +165,47 @@ void SteadyStateGA::Selection1(Solution& p) {
     p = population[r];
 }
 
+void SteadyStateGA::Selection2(Solution& p) {
+    double point = static_cast< double >(std::rand()) * sumOfFitnesses / RAND_MAX;
+    double sum = 0;
+    for (int i = 0; i < PSIZE; ++i) {
+        sum += adjustedFitnesses[i];
+        if (point < sum) {
+            p = population[i];
+            return;
+        }
+    }
+}
+
+void SteadyStateGA::Selection3(Solution& p) {
+    int r1 = std::rand() % PSIZE;
+    int r2 = std::rand() % PSIZE;
+    const Solution& p1 = population[r1];
+    const Solution& p2 = population[r2];
+    int r = static_cast< double >(std::rand()) / RAND_MAX;
+    if (TOURNAMENT_SELECTION_PRESSURE > r) {
+        p = (p1.Fitness >= p2.Fitness) ? p1 : p2;
+    } else {
+        p = (p1.Fitness >= p2.Fitness) ? p2 : p1;
+    }
+}
+
+void SteadyStateGA::Selection4(Solution& p) {
+}
+
 // combine the given parents p1 and p2
 // and store the generated solution at c
 // currently the child will be same as p1
 void SteadyStateGA::Crossover1(const Solution& p1, const Solution& p2, Solution& c) {
-    c = p1;
-    int p = std::rand() % solutionLen;
-    int secondIndex = p;
-    for (int i = p; i < solutionLen + p; ++i) {
-        if (std::find(p1.Chromosome.begin(), p1.Chromosome.begin() + p, p2.Chromosome[i % solutionLen]) == p1.Chromosome.begin() + p) {
-            c.Chromosome[secondIndex++] = p2.Chromosome[i % solutionLen];
+    int point = std::rand() % solutionLen;
+    std::vector< int >::const_iterator p1PointIterator = p1.Chromosome.begin() + point;
+    std::vector< int >::iterator cInsertIterator = std::copy(p1.Chromosome.begin(), p1PointIterator, c.Chromosome.begin());
+    for (int i = 0; i < solutionLen; ++i) {
+        if (std::find(p1.Chromosome.begin(), p1PointIterator, p2.Chromosome[(i + point) % solutionLen]) == p1PointIterator) {
+            cInsertIterator = c.Chromosome.insert(cInsertIterator, p2.Chromosome[(i + point) % solutionLen]);
         }
     }
+    Normalize(c);
     CALL_MEMBER_FN(*this, Evaluate)(c);
 }
 
@@ -172,6 +238,9 @@ void SteadyStateGA::GA() {
         Solution p1(solutionLen);
         Solution p2(solutionLen);
         Solution c(solutionLen);
+        if (Preprocess) {
+            CALL_MEMBER_FN(*this, Preprocess)();
+        }
         CALL_MEMBER_FN(*this, Selection)(p1);
         CALL_MEMBER_FN(*this, Selection)(p2);
         CALL_MEMBER_FN(*this, Crossover)(p1, p2, c);
@@ -202,6 +271,15 @@ void SteadyStateGA::PrintSolution(const Solution& s) {
     std::cout << std::endl;
 }
 
+void SteadyStateGA::Normalize(Solution& s) {
+    /*
+    std::copy(s.Chromosome.begin(), s.Chromosome.end(), tempSolution.Chromosome.begin());
+    std::vector< int >::iterator zeroIter = std::find(tempSolution.Chromosome.begin(), tempSolution.Chromosome.end(), 0);
+    std::vector< int >::iterator aaaa = std::copy(zeroIter, tempSolution.Chromosome.end(), s.Chromosome.begin());
+    std::copy(tempSolution.Chromosome.begin(), zeroIter, aaaa);
+    */
+}
+
 int main() {
     // http://en.cppreference.com/w/cpp/numeric/random/rand
     std::srand(std::time(0));
@@ -210,6 +288,7 @@ int main() {
     SteadyStateGA ga(testCase
             , &SteadyStateGA::Evaluate1
             , &SteadyStateGA::GenerateRandomSolution1
+            , NULL
             , &SteadyStateGA::Selection1
             , &SteadyStateGA::Crossover1
             , &SteadyStateGA::Mutation1
