8cee8187e9248d54d95a9f4ea0d3621020b864f2
jsryu21 <jsryu21@snu.ac.kr>
2015-04-17 13:16:30 +0900

    test shell script.

---
+++ hw1/test51.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for i in {0..3}
+do
+    for j in {0..5}
+    do
+        for k in {0..5}
+        do
+            for p in {0..4}
+            do
+                echo "$i $j $k $p"
+                ./ga $i $j $k $p < cycle.in.51
+            done
+        done
+    done
+done
